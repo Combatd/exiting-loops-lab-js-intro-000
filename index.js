fed81c0ae1c470584f205d5ead39e9562e8358fc
Mark Calvelo <combatdstar@gmail.com>
2019-09-01 08:50:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function breakOut(array, changeValue, stopValue) {
   for(var i = 0; i < stopValue; i+=1) {
     if (i == stopValue) { 
-      array[i] = changeValue;
+      
     }
     
-    break;
+    
   }
   return array; // return mutated array
 }
