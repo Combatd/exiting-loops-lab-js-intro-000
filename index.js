4f2505518b3e09171117aff7ef9515993cb8648c
Mark Calvelo <combatdstar@gmail.com>
2019-09-01 02:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function breakOut(array, changeValue, stopValue) {
   for(var i = 0; i < array.length; i+=1) {
     if (i !== stopValue) { 
       array[i] = changeValue;
-    }
+    } else {
       break;
+    }
   }
   return array; // return mutated array
 }
