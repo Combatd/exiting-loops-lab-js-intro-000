9fb06c33612698eafedb5326128b6a2387e2e9e4
Mark Calvelo <combatdstar@gmail.com>
2019-09-01 02:44:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for(var i = 0; i < array.length; i+=1) {
     if (i === stopValue) { // break out of loop condition
-      array[i] = changeValue;
+      // array[i] = changeValue;
       break;
     }
     array[i] = changeValue;
