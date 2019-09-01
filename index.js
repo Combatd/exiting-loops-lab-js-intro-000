8abe54d76e303ecf48ec6a9f45cc9e58d448e53f
Mark Calvelo <combatdstar@gmail.com>
2019-09-01 09:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  let notFound = null; // default value
+  console.log(findFn);
   for (let i = 0; i < array.length; i++) {
     if (array[i] === findFn) {
       return array[i];
